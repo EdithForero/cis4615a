e195dac910d33f8fda16c5341f989261163df199
EdithForero <72670579+EdithForero@users.noreply.github.com>
2020-10-24 16:11:05 -0400

    Update R06_MET08_J.java

--- R06_MET08_J.java
+++ R06_MET08_J.java
@@ -2,7 +2,7 @@
  * Edith Forero
  * Rule 06. Methods (MET)
  * MET05-J. Ensure that constructors do not call overridable methods
- * Noncompliant Code Example
+ * Compliant Code Example
  * CIS4615 HW2
  * *************************************************************
  */
@@ -14,7 +14,7 @@ public class R06_MET08_J {
             doLogic();
         }
 
-        public void doLogic() {
+        public final void doLogic() {
             System.out.println("This is superclass!");
         }
     }
@@ -27,12 +27,15 @@ public class R06_MET08_J {
             super();
             color = "Red";
         }
-
-        public void doLogic() {
-            System.out.println("This is subclass! The color is :" + color);
-            // ...
-        }
     }
+
+    public static void main(String[] args) {
+        SuperClass bc = new SuperClass();
+        // Prints "This is superclass!"
+        SuperClass sc = new SubClass();
+        // Prints "This is superclass!"
+    }
+}
 
     public static void main(String[] args) {
         SuperClass bc = new SuperClass();
