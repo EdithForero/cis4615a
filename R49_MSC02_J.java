bd0578762db26a9899ddca3bbcae141006de73c0
EdithForero <72670579+EdithForero@users.noreply.github.com>
2020-10-24 16:12:07 -0400

    Update R49_MSC02_J.java

--- R49_MSC02_J.java
+++ R49_MSC02_J.java
@@ -1,24 +1,22 @@
 
-/***************************************************************
- * Edith Forero 
- * Rule 49. Miscellaneous (MSC) 
- * MSC02-J. Generate strong random numbers 
- * Noncompliant Code Example
+/** *************************************************************
+ * Edith Forero
+ * Rule 49. Miscellaneous (MSC)
+ * MSC02-J. Generate strong random numbers
+ * Compliant Code 
  * Example CIS4615 HW2
- * **************************************************************/
-
+ * ************************************************************* */
+import java.security.SecureRandom;
 import java.util.Random;
 
 public class R49_MSC02_J {
 
     public static void main(String[] args) {
 
-        Random number = new Random(123L);
-        
+        SecureRandom number = new SecureRandom();
+        // Generate 20 integers 0..20
         for (int i = 0; i < 20; i++) {
-            // Generate another random integer in the range [0, 20]
-            int n = number.nextInt(21);
-            System.out.println(n);
+            System.out.println(number.nextInt(21));
         }
     }
 }
