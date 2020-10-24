1d7226c6e9d09506f16da3c0f686b629abedaced
EdithForero <72670579+EdithForero@users.noreply.github.com>
2020-10-24 15:51:44 -0400

    Update R03_NUM13_J.java

--- R03_NUM13_J.java
+++ R03_NUM13_J.java
@@ -3,9 +3,9 @@
  * ************************************************************
  * Edith Forero
  * Rule 03. Numeric Types and Operations (NUM)
- * NUM13-J. Avoid loss of precision when converting primitive integers to 
+ * NUM13-J. Avoid loss of precision when converting primitive integers to
  *   floating-point
- * Noncompliant Code Example
+ * Compliant Code Example
  * Example CIS4615 HW2
  * *************************************************************
  */
@@ -13,13 +13,13 @@ public class R03_NUM13_J {
 
     public static void main(String[] args) {
 
-        int result = subFloatFromInt(1234567890, 1234567890);
-        // This prints -46, not 0, as may be expected
+        int result = subDoubleFromInt(1234567890, 1234567890);
+        // Works as expected
         System.out.println(result);
 
     }
 
-    public static int subFloatFromInt(int op1, float op2) {
+    public static int subDoubleFromInt(int op1, double op2) {
         return op1 - (int) op2;
     }
 }
